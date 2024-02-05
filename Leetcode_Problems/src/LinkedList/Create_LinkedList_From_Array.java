ac59076cf484a08e76224203edff20c272421b17
OSAMA KAMEL <128108888+OsamaKamel229@users.noreply.github.com>
2024-02-04 20:57:56 -0800

    first file

--- Leetcode_Problems/src/LinkedList/Create_LinkedList_From_Array.java
+++ Leetcode_Problems/src/LinkedList/Create_LinkedList_From_Array.java
@@ -1,4 +1,23 @@
 package LinkedList;
 
 public class Create_LinkedList_From_Array {
+
+    public Node convertArrayToLinkedList(int[] nums){
+        Node head = new Node(-1);
+        head.setNext(new Node (nums[0]));
+
+        Node currNode = head;
+        for (int arrIdx = 1; arrIdx < nums.length; arrIdx++) {
+            currNode.setNext(new Node(nums[arrIdx]));
+            currNode = currNode.getNext();
+        }
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        Create_LinkedList_From_Array linkedList = new Create_LinkedList_From_Array();
+        System.out.println(linkedList.convertArrayToLinkedList(new int[]{1,2,3,4,5,6,7}));
+    }
+
 }
---
+++ Leetcode_Problems/src/LinkedList/Node.java
@@ -0,0 +1,39 @@
+package LinkedList;
+
+public class Node<T>{
+    private T val;
+    private Node<T> next;
+
+    public Node(T val){
+        this.val = val;
+        this.next = null;
+    }
+
+    public Node (T val, Node<T> next){
+        this.val = val;
+        this.next = next;
+    }
+
+    public T getValue(){
+        return this.val;
+    }
+
+    public Node<T> getNext(){
+        return this.next;
+    }
+    public void setVal(T val){
+        this.val = val;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
+    }
+}
